7e485f2b9008cf2de54255d7656c97eefbafc3bc
rohitpechetti <rohitpechetti6@gmail.com>
2026-03-23 20:51:00 +0530

    Updated backend code

--- driver_ai_backend/app.py
+++ driver_ai_backend/app.py
@@ -23,9 +23,9 @@ app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10 MB max upload
 app.config['MAIL_SERVER']         = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
 app.config['MAIL_PORT']           = int(os.environ.get('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS']        = True
-app.config['MAIL_USERNAME']       = os.environ.get('MAIL_USERNAME', 'your_email@gmail.com')
-app.config['MAIL_PASSWORD']       = os.environ.get('MAIL_PASSWORD', 'your_app_password')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', 'your_email@gmail.com')
+app.config['MAIL_USERNAME']       = os.environ.get('MAIL_USERNAME', 'rohitpechetti2@gmail.com')
+app.config['MAIL_PASSWORD']       = os.environ.get('MAIL_PASSWORD', 'nnlh gdyg cera mkkq')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', 'rohitpechetti2@gmail.com')
 
 mail            = Mail(app)
 db              = Database()
